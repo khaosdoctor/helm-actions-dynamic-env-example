1255193257ba72d81cbe466dc66319a4e7bc3cb8
Lucas Santos <lhs.santoss@gmail.com>
2021-06-02 15:46:56 -0300

    Add app tag
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 const config = (() => {
   return {
     'VUE_APP_BACKEND_BASE_URL': 'http://localhost:3000',
+    'VUE_APP_PROJECT_VERSION': 'teste',
   }
 })()
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main">
-    <h1>Welcome to the {{ projectName }} application</h1>
+    <h1>Welcome to the {{ projectName }} {{ projectVersion }} application</h1>
     <h4>Start by choosing one of the elements on the top menu</h4>
   </div>
 </template>
@@ -10,7 +10,8 @@ export default {
   name: 'Home',
   data () {
     return {
-      projectName: process.env.VUE_APP_PROJECT_TITLE
+      projectName: process.env.VUE_APP_PROJECT_TITLE,
+      projectVersion: `[${config.VUE_APP_PROJECT_VERSION}]` || ''
     }
   }
 }
